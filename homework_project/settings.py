b00facffc0d73bf6e1494ccfb1fe26077dc937ca
Orkun Kurul <orkun.kurul@hotmail.com>
2022-12-28 00:03:35 +0300

    Postgres database connected

--- homework_project/settings.py
+++ homework_project/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'homework_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'homework',
+        'USER': 'razortype',
+        'PASSWORD': 'sdJ5rA29EGqfW7DH',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
