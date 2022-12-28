fd85aa9653b3aa731e7038a0e8e00a2be9842168
Orkun Kurul <orkun.kurul@hotmail.com>
2022-12-28 16:09:29 +0300

    gunicorn lib updated

--- homework_project/settings.py
+++ homework_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-if%lw&@)fws_qp2_(6gb5duko8f18f$9ilmgrardj#6s(rpt_x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['24.199.104.52']
+ALLOWED_HOSTS = ['24.199.104.52', '127.0.0.1']
 
 # Application definition
 
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'homework_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'homework',
-        'USER': 'razortype',
-        'PASSWORD': 'sdJ5rA29EGqfW7DH',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'homework',
+#         'USER': 'razortype',
+#         'PASSWORD': 'sdJ5rA29EGqfW7DH',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
