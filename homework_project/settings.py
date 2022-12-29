561a4e8a6ef5d808cea893178fdd3242ceaf4c71
Orkun Kurul <orkun.kurul@hotmail.com>
2022-12-29 15:08:16 +0300

    database attributes changed

--- homework_project/settings.py
+++ homework_project/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'homework_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'homework',
-#         'USER': 'razortype',
-#         'PASSWORD': 'sdJ5rA29EGqfW7DH',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'homework',
+        'USER': 'razortype',
+        'PASSWORD': 'sdJ5rA29EGqfW7DH',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
