bd6773030749a3e5d623bcb46a06c81ab0a4f56e
fatihbahadir <hunkarhyme@gmail.com>
2022-12-30 21:04:16 +0300

    Footer updated,Navbar updated,Responsive navbar arrangemenets, Frontend and Backend section created

--- homework_project/static/partials/js/_header.js
+++ homework_project/static/partials/js/_header.js
@@ -1,11 +1,37 @@
 let nav=document.querySelector('nav')
-
+let brand=document.querySelector('.navbar-brand')
+let link=document.querySelectorAll('.header__list>li>a')
+let toggle=document.querySelector('.navbar-toggler')
+toggle.addEventListener('click',showNav)
 window.addEventListener("scroll",()=>{
-    if(window.scrollY>30){
+    if(window.scrollY>375){
         nav.classList.remove('bg-transparent')
-        nav.classList.add('bg-dark')
+        nav.classList.add('bg-darkblue')
+        brand.classList.remove('text-dark')
+        brand.classList.add('text-light')
+        link.forEach((a)=>{
+            a.classList.add('text-light')
+        })
     }
     else{
         nav.classList.add('bg-transparent')
+        brand.classList.remove('text-white')
+        brand.classList.add('text-dark')
+        link.forEach((a)=>{
+            a.classList.remove('text-light')
+        })
     }   
 })
+
+function showNav(){
+    nav.classList.remove('bg-transparent')
+    nav.classList.add('bg-darkblue')
+    brand.classList.remove('text-dark')
+    brand.classList.add('text-light')
+    link.forEach((a)=>{
+        a.classList.add('text-light')
+    })
+
+}  
+
+
