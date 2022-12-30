3f61053a464471395b27e2b52e9097bfac0f38f8
Orkun Kurul <orkun.kurul@hotmail.com>
2022-12-31 01:05:20 +0300

    Registration Feature
    - Registration model form created and respectively model created
    - Attributes for each field were declared
    - Sign up and sign in function are generally implemented
    - Redirections and authentication are implemented after sign up
    - Auth was configured in settings.py file
    - Two forms were displayed in register.html

--- users/forms.py
+++ users/forms.py
@@ -1,26 +1,63 @@
 from django import forms
 
-from .models import Person
+from .models import Person, SignUserModel
 from django.contrib.auth.models import User
+
+from django.utils.translation import gettext_lazy as _
 
 class LoginForm(forms.Form):
     username = forms.CharField(
+        label="Kullanıcı Adı",
         max_length=100,
         widget=forms.TextInput(attrs={'class' : 'form-control'})
         )
     password = forms.CharField(
+        label="Şifre",
         widget=forms.PasswordInput(attrs={'class' : 'form-control', 'password' : forms.PasswordInput()})
         )
 
     def __str__(self):
         return self.username
 
+class SignUserForm(forms.ModelForm):
+    class Meta:
+        model = SignUserModel
+        fields = [
+            "username",
+            "email",
+            "password1",
+            "password2",
+        ]
+
+        widgets = {
+            "password1":forms.widgets.PasswordInput(),
+            "password2":forms.widgets.PasswordInput(),
+        }
+
+        labels = {
+            'username': _("Kullanıcı Adı"),
+            'password1': _("Şifre"),
+            'password2': _("Şifre (Tekrar)")
+        }
+
 class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = '__all__'
 
-class UpdatePersonForm(forms.ModelForm):
+class PersonForm(forms.ModelForm):
     class Meta:
         model = Person
-        fields = '__all__'
+        fields = [
+            "name",
+            "surname",
+            "age",
+            "github_url"
+        ]
+
+        labels = {
+            'name': _("Ad"),
+            'surname': _("Soyad"),
+            'age': _("Yaş"),
+            'github_url': _("Github Hesabı (url)")
+        }
