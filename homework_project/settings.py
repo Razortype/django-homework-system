148ed059dacf15a4d3fdb7979197cd12a5a44a3f
Orkun Kurul <orkun.kurul@hotmail.com>
2023-01-04 16:51:48 +0300

    message fix and setting debug adjust to false

--- homework_project/settings.py
+++ homework_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ankaweb.dev', 'www.ankaweb.dev', '24.199.104.52', '127.0.0.1',]
 
