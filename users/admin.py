7e4f0347516cf47823613e1aec3ab1fd9c9e214e
Orkun Kurul <orkun.kurul@hotmail.com>
2023-01-06 01:08:49 +0300

    Password Forgot Feature
    - Password generator for each request complete
    - Email request page created
    - Token request page created
    - New password page created
    - UserToken model created
    - Change password token expire date configured as 15 minute
    - Password generator create password with respected to not existing recursive algorithm
    - Redirection messages implemented for each request and error
    - Password is sending via email, new email sending method created
    - Utiliy files will be generated as app-based login

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Person, CustomUser
+from .models import Person, CustomUser, UserToken
 # Register your models here.
 
 admin.site.register(CustomUser)
 admin.site.register(Person)
+admin.site.register(UserToken)
--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('login', views.LoginView.as_view(), name="login"),
     path('logout', views.LogoutView.as_view(), name="logout"),
     path('register', views.Register.as_view(), name="register"),
-    path('activate-user/<uidb64>/<token>', views.ActivateUser.as_view(), name="activate")
+
+    path('users/forgot-password', views.ForgotPasswordGenerate.as_view(), name="forgot_pw"),
+    path('users/forgot-password/token', views.ForgotPasswordToken.as_view(), name="forgot_pw_token"),
+    path('users/forgot-password/new', views.ForgotPassword.as_view(), name="forgot_pw_nw"),
+    path('activate-user/<uidb64>/<token>', views.ActivateUser.as_view(), name="activate"),
 ]
