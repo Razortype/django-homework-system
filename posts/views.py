bec6821e0352e5154c5f91b417e4bcb349298311
Orkun Kurul <orkun.kurul@hotmail.com>
2023-01-09 21:04:07 +0300

    Post Section Data Management Complete
    - Several Bugs Fixed
    	- Wrong redirection fixed
    	- Html form action bugs fixed
    	- Urls variables changed
    - Views Sections Filled
    	- Update Post data validation created
    	- Delete Post data validation created
    	- Redirection and messages created for each validation
    - homework_list and homework_detail html files modified
    	- Detail page url changed to 'homework/id/<id>/detail', homework list url change with respected to new url
    	- In homework_detail page, new post section created for user. It changes by depending on user submitted or not
    	- Also command for user's post moved to inner post section

--- posts/views.py
+++ posts/views.py
@@ -66,17 +66,17 @@ class HomeworkDetailById(LoginRequiredMixin, View):
 
     post_form = PostForm
 
-    def get(self, req, id, *args, **kwargs):
+    def get(self, req, _id, *args, **kwargs):
         user = req.user
-        homework = Homework.objects.get(pk=id)
+        homework = Homework.objects.get(pk=_id)
 
         content = {
             'title': f'WEB | Ödevler - {homework.name}',
             'homework' : homework,
             'details': HomeworkDetail.objects.filter(homework__id = homework.id),
             'posts': sorted(Post.objects.filter(homework__id = homework.id), key=lambda person: get_timestamp(person.post_at)),
-            'posted': False,
-            'update_form': self.post_form(),
+            'form': self.post_form(),
+            'personpost': None,
             'style_file': 'posts/css/homework_detail.css',
             'js_files': [
                 'partials/js/_navbar.js',
@@ -86,20 +86,24 @@ class HomeworkDetailById(LoginRequiredMixin, View):
         if not isinstance(user, AnonymousUser):
             content['person'] = Person.objects.get(pk=user.id)
             if content['person'] in [i.person for i in content["posts"]]:
-                content['posted'] = True
+                content['personpost'] = Post.objects.get(person = content['person'])
 
         return HttpResponse(render(req, 'posts/homework_detail.html', content))
+
+    def post(self, req, *args, **kwargs):
+        return HttpResponseRedirect('/homeworks')
 
 class HomeworkPostNew(View):
 
     post_form = PostForm
 
-    def post(self, req, id, *args, **kwargs):
+    def post(self, req, _id, *args, **kwargs):
 
         user = req.user
         person = Person.objects.get(pk=user.pk)
+
         try:
-            homework = Homework.objects.get(pk=id) 
+            homework = Homework.objects.get(pk=_id) 
         except Exception:
             homework = None
 
@@ -107,26 +111,74 @@ class HomeworkPostNew(View):
             messages.warning(req, "Uygun ödev bulunamamıştır")
             return HttpResponseRedirect('/homeworks')
 
+        homework_page_url = f'/homeworks/id/{homework.pk}/detail'
+
         post_form = self.post_form(req.POST)
         if not post_form.is_valid():
             messages.warning(req, "Uygun veriler gönderilmedi")
-            return HttpResponseRedirect(f'homeworks/id/{id}')
+            return HttpResponseRedirect(homework_page_url)
 
         if not post_form.cleaned_data['github_url'].startswith(person.github_url):
             messages.warning(req, "Gönderilen ödev size ait olamak zorundadır")
-            return HttpResponseRedirect(f'homeworks/id/{id}')
+            return HttpResponseRedirect(homework_page_url)
 
         post = Post.objects.create(person=person, homework=homework, post_url=post_form.cleaned_data['github_url'])
         post.save()
         messages.success(req, "Post başarıyla atıldı")
-        return HttpResponseRedirect(f'homeworks/id/{id}')
+        return HttpResponseRedirect(homework_page_url)
 
 class HomeworkPostUpdate(View):
-    def post(self, req, *args, **kwargs):
 
-        pass
+    update_post = PostForm
+
+    def post(self, req, homework_id, post_id, *args, **kwargs):
+
+        update_form = self.update_post(req.POST)
+        return_url = f'/homeworks/id/{homework_id}/detail'
+
+        try:
+            post = Post.objects.get(pk=post_id)
+        except Exception:
+            post = None
+
+        if post is None:
+            messages.warning(req, "Aranılan post bulunamadı")
+            return HttpResponseRedirect(return_url)
+
+        if post.person != req.user.person:
+            messages.warning(req, "Postu atan kişi siz olmak zorundasınız")
+            return HttpResponseRedirect(return_url)
+
+        if not update_form.is_valid():
+            messages.warning(req, "Girilen veriler geçerli değildir")
+            return HttpResponseRedirect(return_url)
+
+        if not update_form.cleaned_data['github_url'].startswith(req.user.person.github_url):
+            messages.warning(req, "Gönderilen ödev size ait olamak zorundadır")
+            return HttpResponseRedirect(return_url)
+
+        post.post_url = update_form.cleaned_data['github_url']
+        post.save()
+        messages.success(req, 'Post başarıyla değiştirildi')
+        return HttpResponseRedirect(return_url)
 
 class HomeworkPostDelete(View):
-    def post(self, req, *args, **kwargs):
+    def get(self, req, homework_id, post_id, *args, **kwargs):
 
-        pass
+        return_url = f'/homeworks/id/{homework_id}/detail'
+        try:
+            post = Post.objects.get(pk=post_id)
+        except Exception:
+            post = None
+
+        if post is None:
+            messages.warning(req, "Aranılan post bulunamadı")
+            return HttpResponseRedirect(return_url)
+
+        if post.person != req.user.person:
+            messages.warning(req, "Postu atan kişi siz olmak zorundasınız")
+            return HttpResponseRedirect(return_url)
+
+        post.delete()
+        messages.success(req, "Post başarıyla silindi")
+        return HttpResponseRedirect(return_url)
