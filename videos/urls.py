d4f77c7fd8e30d0b0747e78f7277eed69421c478
Orkun Kurul <orkun.kurul@hotmail.com>
2023-02-14 22:22:05 +0300

    video category urls added

--- videos/urls.py
+++ videos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('videos', views.VideoList.as_view(), name="video_list"),
+    path('videos/<str:category>', views.VideoListByType.as_view(), name="video_list_by_type")
 ]
--- videos/views.py
+++ videos/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from posts.models import Category
 from users.models import Person
 from .models import Video
 
@@ -23,7 +24,37 @@ class VideoList(LoginRequiredMixin, View):
             'videos': Video.objects.all(),
             'style_file': 'videos/css/video_list.css',
             'js_files': [
-                'partials/js/_navbar.js'
+                'partials/js/_navbar.js',
+                'videos/js/video_list.js'
+            ]
+        }
+
+        if not isinstance(user, AnonymousUser):
+            content['person'] = Person.objects.get(pk=user.id)
+
+        return HttpResponse(render(req, 'videos/video_list.html', content))
+
+class VideoListByType(LoginRequiredMixin, View):
+
+    login_url = "login"
+    redirect_field_name = "next"
+
+    def get(self, req, category ,*args, **kwargs):
+
+        user = req.user
+
+        if not Category.objects.all().filter(name=category).exists():
+            messages.error(req, f"Uygun kategori bulunamadı: {category}")
+            return HttpResponseRedirect("/videos")
+
+        content = {
+            'title': 'WEB | Videolar',
+            'categories': Category.objects.all(),
+            'videos': Video.objects.filter(category__name=category),
+            'style_file': 'videos/css/video_list.css',
+            'js_files': [
+                'partials/js/_navbar.js',
+                'videos/js/video_list.js'
             ]
         }
 
